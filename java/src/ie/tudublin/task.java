61802a036ea1eb43a58095c934974117e8ac1898
JamieHeffo <98745575+JamieHeffo@users.noreply.github.com>
2022-04-26 00:19:20 +0100

    step 1 complete task.java

---
+++ java/src/ie/tudublin/task.java
@@ -0,0 +1,51 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+//Make a class called Task that encapsulates the columns from a single row from the tasks.csv file
+public class task {
+    
+    //Make private fields and public accessor methods.
+    private String name;
+    private int start;
+    private int end;
+
+    //create public accessor methods
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getStart() {
+        return start;
+    }
+    public void setStart(int start) {
+        this.start = start;
+    }
+    public int getEnd() {
+        return end;
+    }
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    //Make a constructor that takes initial values and assigns them to the fields.
+    public task(String name, int start, int end) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+    }
+    
+    //Make a constructor that takes a Processing TableRow field as a parameter and make an appropriate toString method.
+    public String toString() {
+        return "\nTask : " + end + "\nName : " + name + "\nStart : " + start + "\n";
+    }
+
+    
+
+    
+
+
+    
+}
